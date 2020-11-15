511f97067a297fe8f6104bedfc7d9bc637029339
kim-taewooo <kim.taewooo@navercorp.com>
2020-11-15 22:54:19 +0900

    챕터 4까지 완료

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
+const isEmail = require("isemail");
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
 const { createStore } = require("./utils");
-const resolvers = require('./resolvers');
+const resolvers = require("./resolvers");
 
 const LaunchAPI = require("./datasources/launch");
 const UserAPI = require("./datasources/user");
@@ -16,6 +17,16 @@ const server = new ApolloServer({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({ store }),
   }),
+  context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = (req.headers && req.headers.authorization) || "";
+    const email = Buffer.from(auth, "base64").toString("ascii");
+    if (!isEmail.validate(email)) return { user: null };
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = (users && users[0]) || null;
+    return { user: { ...user.dataValues } };
+  },
 });
 
 server.listen().then(() => {
